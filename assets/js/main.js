600353b5a53642feba16917e8d829f10430149b3
Andrea <andreamarongiu05@gmail.com>
2021-04-12 16:18:03 +0200

    Inserimento bandiere tramite countryflag.io

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,21 +2,31 @@ var app = new Vue({
   el: '#root',
   data: {
     search: '',
-    filmSearched: ''
+    filmSearched: '',
+    flag: 'https://www.countryflags.io/',
   },
 
   mounted() {
-    // TODO:
+
   },
   methods: {
     ricerca: function () {
-      axios.get('https://api.themoviedb.org/3/search/movie?api_key=a921d6aadd798d788a4bb0455b772f57&query=' + this.search + '&language=IT')
+      if (this.search != '') {
+        axios.get('https://api.themoviedb.org/3/search/movie?api_key=a921d6aadd798d788a4bb0455b772f57&query=' + this.search + '&language=IT')
         .then((response) => {
           this.filmSearched = response.data.results;
-          console.log(this.filmSearched);
+
+          // Cambio per adattare la bandiera al sito
+          this.filmSearched.forEach((item, i) => {
+            if (item.original_language == 'en') {
+              item.original_language = 'gb';
+            }
+          });
+
         })
         // Svuoto input
         this.search = ''
-    }
+      }
+    },
   }
 })
