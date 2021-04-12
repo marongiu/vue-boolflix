accc9dc69b538829e562f67dc64e004ac27266a4
Andrea <andreamarongiu05@gmail.com>
2021-04-12 13:05:43 +0200

    Search input funzionante al click

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,13 +10,10 @@ var app = new Vue({
   },
   methods: {
     ricerca: function () {
-      axios.get('https://api.themoviedb.org/3/search/movie?api_key=a921d6aadd798d788a4bb0455b772f57&query="Avatar"&language=IT')
+      axios.get('https://api.themoviedb.org/3/search/movie?api_key=a921d6aadd798d788a4bb0455b772f57&query=' + this.search + '&language=IT')
         .then((response) => {
           this.filmSearched = response.data.results;
-          console.log(this.filmSearched);
         })
-
-        console.log(this.search);
     }
   }
 })
