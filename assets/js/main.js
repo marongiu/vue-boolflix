d19f08bd1e31157d237f49d7000efdf91a78240b
Andrea <andreamarongiu05@gmail.com>
2021-04-12 15:33:16 +0200

    Fine milestone1

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,6 +13,7 @@ var app = new Vue({
       axios.get('https://api.themoviedb.org/3/search/movie?api_key=a921d6aadd798d788a4bb0455b772f57&query=' + this.search + '&language=IT')
         .then((response) => {
           this.filmSearched = response.data.results;
+          console.log(this.filmSearched);
         })
     }
   }
