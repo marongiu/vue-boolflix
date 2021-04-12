6565c9aa46b7c6d2a136c7189e2d3d28c311b958
Andrea <andreamarongiu05@gmail.com>
2021-04-12 12:43:09 +0200

    Inizio prove con axios

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,22 @@
 var app = new Vue({
   el: '#root',
   data: {
-
+    search: '',
+    filmSearched: ''
   },
 
+  mounted() {
+    // TODO:
+  },
   methods: {
-    
+    ricerca: function () {
+      axios.get('https://api.themoviedb.org/3/search/movie?api_key=a921d6aadd798d788a4bb0455b772f57&query="Avatar"&language=IT')
+        .then((response) => {
+          this.filmSearched = response.data.results;
+          console.log(this.filmSearched);
+        })
+
+        console.log(this.search);
+    }
   }
 })
