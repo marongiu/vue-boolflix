037b06c6bbb60a7dee76a9362dcde1c6bce7cd41
Andrea <andreamarongiu05@gmail.com>
2021-04-13 11:01:52 +0200

    Carico sia le serie che i film con axios all

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,8 @@ var app = new Vue({
   el: '#root',
   data: {
     search: '',
-    filmSearched: '',
+    filmSearched: [],
+    serieSearched: [],
     flag: 'https://www.countryflags.io/',
   },
 
@@ -11,21 +12,35 @@ var app = new Vue({
   },
   methods: {
     ricerca: function () {
-        axios.get('https://api.themoviedb.org/3/search/movie?api_key=a921d6aadd798d788a4bb0455b772f57&query=' + this.search + '&language=IT')
-        .then((response) => {
-          this.filmSearched = response.data.results;
-
-          // Cambio per adattare la bandiera al sito
-          this.filmSearched.forEach((item, i) => {
-            if (item.original_language == 'en') {
-              item.original_language = 'gb';
-            }
-          });
-        }) // fine chiamata axio
+      const movieReq = axios.get('https://api.themoviedb.org/3/search/movie?api_key=a921d6aadd798d788a4bb0455b772f57&query=' + this.search + '&language=IT');
+      const serieReq = axios.get('https://api.themoviedb.org/3/search/tv?api_key=a921d6aadd798d788a4bb0455b772f57&language=it_IT&query=' + this.search);
+      axios.all([
+        movieReq,
+        serieReq
+      ])
+      .then(axios.spread((film, serie) => {
 
 
-        // Svuoto input
-        this.search = ''
+        this.filmSearched =  film.data.results;
+        this.serieSearched = serie.data.results;
+
+        // Cambio per adattare la bandiera al sito per film
+        this.filmSearched.forEach((item, i) => {
+          if (item.original_language == 'en') {
+            item.original_language = 'gb';
+          }
+        });
+          // Cambio per adattare la bandiera al sito per film
+        this.serieSearched.forEach((item, i) => {
+          if (item.original_language == 'en') {
+            item.original_language = 'gb';
+          }
+        });
+
+
+      }))
+      // Svuoto input
+      this.search = ''
     },
   }
 })
