95ca1f2c596a95691a842493b748565d6d9d5304
Andrea <andreamarongiu05@gmail.com>
2021-04-14 11:03:42 +0200

    Inserisco stelline sottraendole dal primo v-for

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,48 +13,53 @@ var app = new Vue({
   },
   methods: {
     ricerca: function () {
-      const movieReq = axios.get('https://api.themoviedb.org/3/search/movie?api_key=a921d6aadd798d788a4bb0455b772f57&query=' + this.search);
-      const serieReq = axios.get('https://api.themoviedb.org/3/search/tv?api_key=a921d6aadd798d788a4bb0455b772f57&language=it_IT&query=' + this.search);
-      axios.all([
-        movieReq,
-        serieReq
-      ])
-      .then(axios.spread((film, serie) => {
+      if (this.search.length >= 1) {
+        const movieReq = axios.get('https://api.themoviedb.org/3/search/movie?api_key=a921d6aadd798d788a4bb0455b772f57&query=' + this.search);
+        const serieReq = axios.get('https://api.themoviedb.org/3/search/tv?api_key=a921d6aadd798d788a4bb0455b772f57&language=it_IT&query=' + this.search);
+        axios.all([
+          movieReq,
+          serieReq
+        ])
+        .then(axios.spread((film, serie) => {
 
 
-        this.filmSearched = film.data.results;
-        this.serieSearched = serie.data.results;
-        // Cambio per adattare la bandiera al sito per film
-        this.filmSearched.forEach((item, i) => {
-          if (item.original_language == 'en') {
-            item.original_language = 'gb';
-          }
-        });
+          this.filmSearched = film.data.results;
+          this.serieSearched = serie.data.results;
           // Cambio per adattare la bandiera al sito per film
-        this.serieSearched.forEach((item, i) => {
-          if (item.original_language == 'en') {
-            item.original_language = 'gb';
-          }
-        });
-        // Richiamo la funzione per modificare la valutazione
-        this.vote()
-
-      }))
-      // Svuoto input
-      this.search = ''
+          this.flagFix();
+          // Richiamo la funzione per modificare la valutazione
+          this.vote();
+        }))
+        // Svuoto input
+        this.search = ''
+      }
     },
-
+// Arrotondo il voto e lo trasformo da 1 a 5
     vote: function () {
       this.filmSearched.forEach((item, i) => {
         item.vote_average = Math.ceil(item.vote_average * 5 / 10);
             console.log(item.vote_average);
-          return item.vote_average;
       });
 
       this.serieSearched.forEach((item, i) => {
         item.vote_average = Math.ceil(item.vote_average * 5 / 10);
+        console.log(item.vote_average);
       });
 
+    },
+// Sistemo il codice flag per adattarlo al sito
+    flagFix: function () {
+      this.filmSearched.forEach((item, i) => {
+        if (item.original_language == 'en') {
+          item.original_language = 'gb';
+        }
+      });
+        // Cambio per adattare la bandiera al sito per film
+      this.serieSearched.forEach((item, i) => {
+        if (item.original_language == 'en') {
+          item.original_language = 'gb';
+        }
+      });
     }
 
   }
